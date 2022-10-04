36001011074a75d56837a3725dc1bcd1b2b79e2f
gumelar <gumelar.wicaksono6@gmail.com>
2022-10-04 20:22:54 +0700

    testing deploy from heroku

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
           <br />
-          testing deploy from
+          testing deploy from heroku
         </p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
