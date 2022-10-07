67a01f4d27a50ea62674483951fdac2aca429b7f
gumelar <gumelar.wicaksono6@gmail.com>
2022-10-08 02:18:04 +0700

    create class and func use props and state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Component from './component/Index';
+
 function App() {
   return (
     <div className="App">
-      <p>membuat halaman pertama saya dengan react app</p>
+      <Component />
     </div>
   );
 }
