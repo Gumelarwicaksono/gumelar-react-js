93fe6dafd1aa8147068fdb6912a105889f6ba499
gumelar <gumelar.wicaksono6@gmail.com>
2022-10-07 20:08:53 +0700

    create class and func use state and props

--- src/component/sub_componen/classCoponen.jsx
+++ src/component/sub_componen/classCoponen.jsx
@@ -3,70 +3,46 @@ import './Style.css';
 class classComponen extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = {
-      value1: 0,
-      value2: 0,
-      hasil: 0,
+      number1: '',
+      number2: '',
+      total: '',
     };
+  }
+  handlenumber1 = (event) => {
+    this.setState({ number1: event.target.value });
+  };
+  handlenumber2 = (event) => {
+    this.setState({ number2: event.target.value });
+  };
 
-    this.plusHendler = this.plusHendler.bind(this);
-    this.minusHendler = this.minusHendler.bind(this);
-    this.plusHendlerDua = this.plusHendlerDua.bind(this);
-    this.minusHendlerDua = this.minusHendlerDua.bind(this);
-    this.hasil = this.hasil.bind(this);
-  }
-  plusHendler() {
-    this.setState({ value1: this.state.value1 + 1 });
-  }
-  minusHendler() {
-    if (this.state.value1 > 0) {
-      this.setState({ value1: this.state.value1 - 1 });
-    }
-  }
-  // value dua
-  plusHendlerDua() {
-    this.setState({ value2: this.state.value2 + 1 });
-  }
-  minusHendlerDua() {
-    if (this.state.value2 > 0) {
-      this.setState({ value2: this.state.value2 - 1 });
-    }
-  }
-  // hasil
-  hasil() {
-    this.setState({ hasil: this.state.value1 + this.state.value2 });
-  }
+  exe = (e) => {
+    e.preventDefault();
+    this.setState({ total: parseInt(this.state.number1) + parseInt(this.state.number2) });
+  };
   render() {
     return (
       <div>
         <div className="nav">
           <h1>{this.props.nama}</h1>
         </div>
-        <div className="container">
-          <h3>penjumlahan mengunakan state in classComponen</h3>
-          <div>
-            <div className="dis">
-              <p>nilai 1 = </p>
-              <button onClick={this.minusHendler}>-</button>
-              <h4 style={{ color: 'red' }}>{this.state.value1}</h4>
-              <button onClick={this.plusHendler}>+</button>
+        <div>
+          <h1>Simple Calculator</h1>
+          <p>MEMBUAT KALKULATOR</p>
+          <form onSubmit={this.exe}>
+            <div>
+              Number 1:
+              <input type="text" value={this.state.number1} onChange={this.handlenumber1} />
             </div>
-            <br />
-            <div className="dis">
-              <p>nilai 2 = </p>
-              <button onClick={this.minusHendlerDua}>-</button>
-              <h4 style={{ color: 'red' }}>{this.state.value2}</h4>
-              <button onClick={this.plusHendlerDua}>+</button>
+            <div>
+              Number 2:
+              <input type="text" value={this.state.number2} onChange={this.handlenumber2} />
             </div>
-            <br />
-            <div className="dis">
-              <button className="btn" onClick={this.hasil}>
-                hasil{' '}
-              </button>
-              <h4 style={{ color: '#367E18' }}> = {this.state.hasil}</h4>
+            <div>
+              <button type="submit"> Add </button>
             </div>
-          </div>
+          </form>
+          <input type="text" name="name" value={this.state.total} />
         </div>
       </div>
     );
