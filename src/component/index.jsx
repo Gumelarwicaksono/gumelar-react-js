15abbd2eb21cc31c89432260cbd42f9c1511f9a0
gumelar <gumelar.wicaksono6@gmail.com>
2022-10-07 20:11:13 +0700

    create class and func use state and props

--- src/component/index.jsx
+++ src/component/index.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import ClassComponen from './sub_componen/ClassCoponen';
 import FunctionalComponen from './sub_componen/FunctionalComponen';
 
 export default class component extends React.Component {
@@ -7,7 +6,6 @@ export default class component extends React.Component {
     const mern = ['mongodb', 'exspres', 'react', 'nodejs'];
     return (
       <>
-        <ClassComponen nama="gumelar wicaksono this props in classcomponen" />
         <FunctionalComponen nama={mern} />
       </>
     );
